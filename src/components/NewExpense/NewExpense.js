457acfad2aa5ab27df77b3f4f51de3fe6fa66fef
AdityaSingh25 <richiaditya25@gmail.com>
2023-05-12 04:03:45 +0530

    yarn switch

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -2,9 +2,15 @@ import React from "react";
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 const NewExpense = () => {
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id:Math.random().toString()
+    };
+  };
   return (
     <div className="new-expense">
-      <ExpenseForm />
+      <ExpenseForm onSaveExpenseData />
     </div>
   );
 };
